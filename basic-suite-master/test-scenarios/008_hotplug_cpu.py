3e9e5ce697da7e0567aaad8397fa886469bc5ae3
vmakarova <makarovavs@mail.ru>
2017-06-05 12:48:38 +0300

    ssh to vm0 using paramiko

--- basic-suite-master/test-scenarios/008_hotplug_cpu.py
+++ basic-suite-master/test-scenarios/008_hotplug_cpu.py
@@ -1,4 +1,5 @@
 import nose.tools as nt
+import paramiko
 
 from ovirtsdk.xml import params
 from ovirtlago import testlib
@@ -20,6 +21,20 @@ def hotplug_cpu(api):
         vm.update()
     )
     nt.assert_true(api.vms.get(VM0_NAME).cpu.topology.sockets == 2)
+    
+    host = '192.168.201.213'
+    user = 'cirros'
+    secret = 'cubswin:)'
+    port = 22
+    
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(hostname=host, username=user, password=secret, port=port)
+    stdin, stdout, stderr = client.exec_command('lscpu | grep CPU\'(\'s\')\':')
+    cpu_number = stdout.read().splitlines()[0].split(":")[1].strip()
+    client.close()
+    nt.assert_true(int(cpu_number) == 2)
+
 
 
 _TEST_LIST = [
